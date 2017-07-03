1c88f15b31a26ba6710ef159b688e4521a8619e4
Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2017-07-03 13:14:19 +0300

    Work in progress - Need to address the TODO in order to move forward.

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/RoadAddressChangeInfoMapper.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/RoadAddressChangeInfoMapper.scala
@@ -12,18 +12,26 @@ object RoadAddressChangeInfoMapper extends RoadAddressMapper {
   private def createAddressMap(sources: Seq[ChangeInfo]): Seq[RoadAddressMapping] = {
     val pseudoGeom = Seq(Point(0.0, 0.0), Point(1.0, 0.0))
     sources.map(ci => {
-          ci.changeType match {
-            case 1 =>
-              logger.debug("Change info> oldId: "+ci.oldId+" newId: "+ci.newId+" changeType: "+ci.changeType)
-              Some(RoadAddressMapping(ci.oldId.get, ci.newId.get, ci.oldStartMeasure.get, ci.oldEndMeasure.get,
-              ci.newStartMeasure.get, ci.newEndMeasure.get, pseudoGeom, pseudoGeom, Some(ci.vvhTimeStamp)))
-            case 2 =>
-              logger.debug("Change info> oldId: "+ci.oldId+" newId: "+ci.newId+" changeType: "+ci.changeType)
-              Some(RoadAddressMapping(ci.oldId.get, ci.newId.get, ci.oldStartMeasure.get, ci.oldEndMeasure.get,
-                ci.newStartMeasure.get, ci.newEndMeasure.get, pseudoGeom, pseudoGeom, Some(ci.vvhTimeStamp)))
-            case _ => None
-          }
-        }).filter(c => c.isDefined).map(_.get)
+      ci.changeType match {
+        case 1 =>
+          logger.debug("Change info> oldId: "+ci.oldId+" newId: "+ci.newId+" changeType: "+ci.changeType)
+          Some(RoadAddressMapping(ci.oldId.get, ci.newId.get, ci.oldStartMeasure.get, ci.oldEndMeasure.get,
+            ci.newStartMeasure.get, ci.newEndMeasure.get, pseudoGeom, pseudoGeom, Some(ci.vvhTimeStamp)))
+        case 2 =>
+          logger.debug("Change info> oldId: "+ci.oldId+" newId: "+ci.newId+" changeType: "+ci.changeType)
+          Some(RoadAddressMapping(ci.oldId.get, ci.newId.get, ci.oldStartMeasure.get, ci.oldEndMeasure.get,
+            ci.newStartMeasure.get, ci.newEndMeasure.get, pseudoGeom, pseudoGeom, Some(ci.vvhTimeStamp)))
+        case 5 =>
+          logger.debug("Change info> oldId: "+ci.oldId+" newId: "+ci.newId+" changeType: "+ci.changeType)
+          Some(RoadAddressMapping(ci.oldId.get, ci.newId.get, ci.oldStartMeasure.get, ci.oldEndMeasure.get,
+            ci.newStartMeasure.get, ci.newEndMeasure.get, pseudoGeom, pseudoGeom, Some(ci.vvhTimeStamp)))
+        case 6 =>
+          logger.debug("Change info> oldId: "+ci.oldId+" newId: "+ci.newId+" changeType: "+ci.changeType)
+          Some(RoadAddressMapping(ci.oldId.get, ci.newId.get, ci.oldStartMeasure.get, ci.oldEndMeasure.get,
+            ci.newStartMeasure.get, ci.newEndMeasure.get, pseudoGeom, pseudoGeom, Some(ci.vvhTimeStamp)))
+        case _ => None
+      }
+    }).filter(c => c.isDefined).map(_.get)
   }
 
   private def applyChanges(changes: Seq[Seq[ChangeInfo]], roadAddresses: Map[Long, Seq[RoadAddress]]): Map[Long, Seq[RoadAddress]] = {
@@ -43,13 +51,26 @@ object RoadAddressChangeInfoMapper extends RoadAddressMapper {
   }
 
   def resolveChangesToMap(roadAddresses: Map[Long, Seq[RoadAddress]], changedRoadLinks: Seq[RoadLink], changes: Seq[ChangeInfo]): Map[Long, Seq[RoadAddress]] = {
-    val sections = partition(roadAddresses.values.toSeq.flatten)
+    val sections = generateSections(roadAddresses, changes)
     val originalAddressSections = groupByRoadSection(sections, roadAddresses.values.toSeq.flatten)
     preTransferCheckBySection(originalAddressSections)
     val groupedChanges = changes.groupBy(_.vvhTimeStamp).values.toSeq
     val appliedChanges = applyChanges(groupedChanges.sortBy(_.head.vvhTimeStamp), roadAddresses)
-    val result = postTransferCheckBySection(groupByRoadSection(sections, appliedChanges.values.toSeq.flatten), originalAddressSections)
+    val grouped = groupByRoadSection(sections, appliedChanges.values.toSeq.flatten)
+    val result = postTransferCheckBySection(grouped, originalAddressSections)
     result.values.toSeq.flatten.groupBy(_.linkId)
+  }
+
+  //TODO: find out how to actually generate the startAddrMValue and endAddrMValue for the division cases, it's clashing with RoadAddressMapper - line 103
+  private def generateSections(roadAddresses: Map[Long, Seq[RoadAddress]], changes: Seq[ChangeInfo]): Seq[RoadAddressSection] ={
+    val roadAddressSeq = roadAddresses.values.toSeq.flatten
+    val sections = partition(roadAddressSeq)
+    val filteredChanges = changes.filter(c => (c.changeType == 5 || c.changeType == 6) && (roadAddresses.values.toSeq.flatten.map(_.linkId).contains(c.oldId.get)))
+    val sects = filteredChanges.flatMap(fi => {
+      val ra = roadAddressSeq.find(_.linkId == fi.oldId.getOrElse(-1))
+      partition(Seq(ra.get.copy(linkId = fi.oldId.get)))
+    })
+    sections ++ sects
   }
 
   private def groupByRoadSection(sections: Seq[RoadAddressSection],
